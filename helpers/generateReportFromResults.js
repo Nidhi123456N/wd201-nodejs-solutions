c47fc5f338404b188b7e7aedfe88ab053f077c6e
Hari Gopal <mail@harigopal.in>
2023-02-21 23:30:15 +0530

    Update static portions of feedback given to students

--- helpers/generateReportFromResults.js
+++ helpers/generateReportFromResults.js
@@ -11,12 +11,12 @@ let generateFeedback = (passed, results) => {
     .join("\n\n");
 
   const prefix = passed
-    ? "Good work! It looks like you've built your webpage according to our specifications. These are the tests we ran:"
+    ? "Good work! It looks like you've completed the assignment according to our specifications. These are the tests we ran:"
     : "Uh oh! It looks like you've missed some parts of the assignment. Here are the results of the tests that we ran. A tick (✓) indicates a successful test, and a cross (✗) indicates a failed test.";
 
   const suffix = passed
     ? "See you in the next level!"
-    : "Please make sure that you go through the assignment instructions. If you're having trouble with this assignment, please reach out to the Pupilfirst team on the Web Development community.";
+    : "Please make sure that you go through the assignment instructions. If you're having trouble with this assignment, please ask for help on the Pupilfirst School Discord server, and include details of the issue you're having.";
 
   const feedback = prefix + "\n\n" + testResults + "\n\n" + suffix;
 
@@ -59,7 +59,7 @@ readFile("results.json").then((data) => {
       grade: "reject",
       status: "failure",
       feedback:
-        "We are unable to test your submission - something about it was too different from what we were expecting. Please check the instructions for this task and try again. If you have seen this message more than once, please reach out to Pupilfirst team for support.",
+        "We are unable to test your submission - something about it was too different from what we were expecting. Please check the instructions for this assignment and try again. If you have seen this message more than once, please reach out to Pupilfirst team for support via our Discord server.",
       report: "Unable to generate report due to missing results.json.",
     });
   }
